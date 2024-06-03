1f66cc3100c739780153c33e412ec0c404c0028c
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2024-06-03 19:02:36 -0400

    Fixing bug where sometimes images won't load when you reload twice

---
+++ NasaImageExplorer/View/NoResultsLabel.swift
@@ -0,0 +1,8 @@
+//
+//  NoResultsLabel.swift
+//  NasaImageExplorer
+//
+//  Created by Micheal Bingham on 6/3/24.
+//
+
+import Foundation
---
+++ NasaImageExplorer/View/WelcomeView.swift
@@ -0,0 +1,8 @@
+//
+//  WelcomeView.swift
+//  NasaImageExplorer
+//
+//  Created by Micheal Bingham on 6/3/24.
+//
+
+import Foundation
